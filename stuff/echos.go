37f2f5add083d278fac49d3147a0dea04a4d2ba9
connorkuljis <conkuljis@gmail.com>
2022-11-10 01:40:36 +0800

    remove whitespace

--- stuff/echos.go
+++ stuff/echos.go
@@ -10,18 +10,15 @@ import (
 
 func main() {
 	start := time.Now()
-
 	echo1()
 	echo2()
 	echo3()
 	echo4()
-
 	fmt.Printf("\n\t\ttotal %fs elapsed\n", time.Since(start).Seconds())
 }
 
 func echo1() {
 	fmt.Print("echo1:\n")
-
 	start := time.Now()
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
@@ -31,13 +28,11 @@ func echo1() {
 		// fmt.Printf("%d : %s\n", i, os.Args[i])
 	}
 	fmt.Println(s)
-
 	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
 }
 
 func echo2() {
 	fmt.Print("echo2: ")
-
 	start := time.Now()
 	s, sep := "", ""
 	// _ is an unused variable for the current index count
@@ -46,24 +41,19 @@ func echo2() {
 		sep = " "
 	}
 	fmt.Println(s)
-
 	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
 }
 
 func echo3() {
 	fmt.Print("echo3: ")
 	start := time.Now()
-
 	fmt.Println(strings.Join(os.Args[1:], " "))
-
 	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
 }
 
 func echo4() {
 	fmt.Print("echo4: ")
 	start := time.Now()
-
 	fmt.Println(os.Args[1:])
-
 	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
 }
