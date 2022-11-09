4e02dd649bd0669561f928dea079d4a51d1ffb4e
connorkuljis <conkuljis@gmail.com>
2022-11-10 01:37:40 +0800

    echo benchmark

---
+++ stuff/echos.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	echo1()
+	echo2()
+	echo3()
+	echo4()
+
+	fmt.Printf("\n\t\ttotal %fs elapsed\n", time.Since(start).Seconds())
+}
+
+func echo1() {
+	fmt.Print("echo1:\n")
+
+	start := time.Now()
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		indexStr := strconv.Itoa(i)
+		s += sep + indexStr + " : " + os.Args[i]
+		sep = "\n"
+		// fmt.Printf("%d : %s\n", i, os.Args[i])
+	}
+	fmt.Println(s)
+
+	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
+}
+
+func echo2() {
+	fmt.Print("echo2: ")
+
+	start := time.Now()
+	s, sep := "", ""
+	// _ is an unused variable for the current index count
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
+}
+
+func echo3() {
+	fmt.Print("echo3: ")
+	start := time.Now()
+
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
+}
+
+func echo4() {
+	fmt.Print("echo4: ")
+	start := time.Now()
+
+	fmt.Println(os.Args[1:])
+
+	fmt.Printf("\t%fs elapsed\n", time.Since(start).Seconds())
+}
---
+++ stuff/myhelloworld.go
@@ -0,0 +1,12 @@
+package main
+
+// import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
+
+// func main() {
+// 	fmt.Println("hello", "✨")
+// 	fmt.Println(add(2, 3))
+// }
