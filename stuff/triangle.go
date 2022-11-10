2b585054837a78b47200fb6fe75c59fe584d0fec
connorkuljis <conkuljis@gmail.com>
2022-11-10 18:06:11 +0800

    printfs

--- stuff/triangle.go
+++ stuff/triangle.go
@@ -1,18 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-// triangular numbers using recursion
+const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
 
 func main() {
-	fmt.Println(triangularRecursive(6))
+	tri(5)
 }
 
+func tri(n int) int {
+	return triangularRecursive(n)
+}
+
+// triangular numbers using recursion
 func triangularRecursive(n int) int {
 	if n == 1 {
+		fmt.Println("hit base case")
+		fmt.Printf("n | val\t[ call ]\n")
 		return n
 	}
+	fmt.Printf("n = %d\n", n)
 	rv := n + triangularRecursive(n-1)
-	fmt.Printf("%d | %d\n", n, rv)
+
+	fmt.Printf("%d | %d \t[ ", n, rv)
+	call := strconv.Itoa(n) + " + tri(" + strconv.Itoa(n-1) + ") ]\n"
+	fmt.Printf(InfoColor, call)
+
 	return rv
 }
